5b73e3cb97de205d7013548cc755705a1478f404
qiaoxingxing <1620998366@qq.com>
2019-12-09 18:16:46 +0800

    修改粘贴时转换ul的格式

--- to-markdown/to-markdown.js
+++ to-markdown/to-markdown.js
@@ -502,11 +502,13 @@ module.exports = [
     filter: 'li',
     replacement: function (content, node) {
       content = content.replace(/^\s+/, '').replace(/\n/gm, '\n    ');
-      var prefix = '*   ';
+      // var prefix = '*   ';
+      var prefix = '- '; //qxx
       var parent = node.parentNode;
       var index = Array.prototype.indexOf.call(parent.children, node) + 1;
 
-      prefix = /ol/i.test(parent.nodeName) ? index + '.  ' : '*   ';
+      // prefix = /ol/i.test(parent.nodeName) ? index + '.  ' : '*   ';
+      prefix = /ol/i.test(parent.nodeName) ? index + '.  ' : '- '; //qxx
       return prefix + content;
     }
   },
